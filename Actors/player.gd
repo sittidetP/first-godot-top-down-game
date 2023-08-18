d84fbb412e95cd6ca7b01dd515046bebb32f34b0
sittidetP <pawutinan_s@silpakorn.edu>
2023-08-18 20:36:30 +0700

    init shoot mechanic in player and edit bullter

--- Actors/player.gd
+++ Actors/player.gd
@@ -2,9 +2,17 @@ class_name Player
 extends CharacterBody2D
 
 @export var speed := 200.0
+@export var bullet : PackedScene
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		print("shoot")
 
 func _physics_process(delta: float) -> void:
+	handle_move()
+	move_and_slide()
+	
+func handle_move():
 	var direction_x = Input.get_axis("move_left", "move_right")
 	var direction_y = Input.get_axis("move_forward", "move_back")
 	velocity = Vector2(direction_x * speed, direction_y * speed)
-	move_and_slide()
