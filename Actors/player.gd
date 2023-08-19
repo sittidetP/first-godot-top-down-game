54340d8e7a9c2f0368dc468f1ea6fe14f9c6fae3
sittidetP <pawutinan_s@silpakorn.edu>
2023-08-19 23:39:19 +0700

    add fire rate in player

--- Actors/player.gd
+++ Actors/player.gd
@@ -3,15 +3,14 @@ extends CharacterBody2D
 
 @export var speed := 200.0
 @export var bullet : PackedScene
+@onready var fire_timer: Timer = $FireTimer
+
+
+var fire_time : float
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("shoot"):
-		var mouse_position := get_global_mouse_position()
-		var shooted_bullet := bullet.instantiate()
-		shooted_bullet.position = global_position
-		var bullet_rad := (mouse_position - global_position).angle()
-		shooted_bullet.rotate(bullet_rad)
-		get_parent().add_child(shooted_bullet)
+	if Input.is_action_just_pressed("shoot") and fire_timer.is_stopped():
+		fire()
 		
 
 func _physics_process(delta: float) -> void:
@@ -22,3 +21,12 @@ func handle_move():
 	var direction_x = Input.get_axis("move_left", "move_right")
 	var direction_y = Input.get_axis("move_forward", "move_back")
 	velocity = Vector2(direction_x * speed, direction_y * speed)
+
+func fire():
+	var mouse_position := get_global_mouse_position()
+	var shooted_bullet := bullet.instantiate()
+	shooted_bullet.position = global_position
+	var bullet_rad := (mouse_position - global_position).angle()
+	shooted_bullet.rotate(bullet_rad)
+	get_parent().add_child(shooted_bullet)
+	fire_timer.start()
