2bdd9970479dea5e50e8a330a07f06d4e5e8d739
sittidetP <pawutinan_s@silpakorn.edu>
2023-08-21 20:47:52 +0700

    add start the game logic

--- Levels/main_level.gd
+++ Levels/main_level.gd
@@ -8,6 +8,7 @@ extends Node
 @onready var enemy_spawn_position1: PathFollow2D = $EnemyPath/EnemySpawnPosition1
 @onready var enemy_spawn_position2: PathFollow2D = $EnemyPath/EnemySpawnPosition2
 @onready var ui_canvas_layer: CanvasLayer = $UICanvasLayer
+@onready var game_name_canvas_layer: CanvasLayer = $GameNameCanvasLayer
 @onready var mob_timer: Timer = $MobTimer
 @onready var player: Player = $Player
 
@@ -19,12 +20,11 @@ const SCORE_STR := "Score : %s"
 func _ready() -> void:
 	Events.score_up.connect(increas_score)
 	mob_timer.stop()
-#	start_game()
 	
 func start_game() -> void:
 	mob_timer.start()
 	ui_canvas_layer.visible = true
-	
+	score = 0
 	score_label.text = SCORE_STR % score
 	start_time = Time.get_ticks_msec()
 	randomize()
@@ -32,6 +32,7 @@ func start_game() -> void:
 func _process(delta: float) -> void:
 	if player == null:
 		mob_timer.stop()
+		game_name_canvas_layer.visible = true
 	else:
 		var current_time := (Time.get_ticks_msec() - start_time) / 1000
 		time_label.text = str(round(current_time))
@@ -53,3 +54,12 @@ func spawn_eneny(enemy_spawner: PathFollow2D, target_position: Vector2) -> void:
 	mob.rotation = (target_position - enemy_spawner.position).angle()
 	mob.position = enemy_spawner.position
 	add_child(mob)
+
+
+func _on_start_button_pressed() -> void:
+	game_name_canvas_layer.visible = false
+	start_game()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
